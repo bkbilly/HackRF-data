a184b92ddfc56b33fc1e02f94767c0db5b1d0b6c
bkbilly <bkbilly@hotmail.com>
2019-12-18 14:31:36 +0000

    added morse encoding

--- morse/top_block.py
+++ morse/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Nov 28 10:43:40 2019
+# Generated: Tue Dec  3 11:32:30 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -16,15 +16,14 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from gnuradio import analog
-from gnuradio import audio
-from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
 from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import waterfallsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
@@ -42,11 +41,25 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 4800000
+        self.samp_rate = samp_rate = 48000
 
         ##################################################
         # Blocks
         ##################################################
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+        	self.GetWin(),
+        	baseband_freq=434.5e6,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="Waterfall Plot",
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
         self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_c(
         	self.GetWin(),
         	title="Scope Plot",
@@ -74,30 +87,21 @@ class top_block(grc_wxgui.top_block_gui):
         self.osmosdr_source_0.set_antenna("", 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
           
-        self.blocks_wavfile_sink_0 = blocks.wavfile_sink("/home/bkbilly/Desktop/morse.wav", 1, samp_rate, 8)
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
-        self.audio_sink_0 = audio.sink(samp_rate, "", True)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, 700, 0.9, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
-        self.connect((self.blocks_complex_to_float_0, 0), (self.audio_sink_0, 0))    
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))    
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_float_0, 0))    
-        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))    
         self.connect((self.osmosdr_source_0, 0), (self.wxgui_scopesink2_0_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.wxgui_waterfallsink2_0, 0))    
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
         self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
 
 
 def main(top_block_cls=top_block, options=None):
