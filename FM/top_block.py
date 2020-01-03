35a6b3db628f50c24220dc2bd53cdc40c03c31e0
bkbilly <bkbilly@hotmail.com>
2020-01-03 19:39:27 +0000

    send morse code with frequency modulation

--- FM/top_block.py
+++ FM/top_block.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Top Block
-# Generated: Tue Dec  3 11:45:22 2019
+# Title: FM radio FFT example
+# Author: David Haworth
+# Generated: Fri Jan  3 18:03:34 2020
 ##################################################
 
 if __name__ == '__main__':
@@ -16,146 +17,334 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from PyQt4 import Qt
 from gnuradio import analog
+from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
-from gnuradio import qtgui
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.wxgui import constsink_gl
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
-import sip
-import sys
 import time
+import wx
 
 
-class top_block(gr.top_block, Qt.QWidget):
+class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Top Block")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Top Block")
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        grc_wxgui.top_block_gui.__init__(self, title="FM radio FFT example")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
-        self.FM_freq = FM_freq = 107.9e6
+        self.volume = volume = 0.2
+        self.variable_static_text_0 = variable_static_text_0 = 'RTL R820T'
+        self.variable_sample_rate_0 = variable_sample_rate_0 = 2.048E6
+        self.transition = transition = 1e6
+        self.quadrature = quadrature = 500e3
+        self.cutoff = cutoff = 100e3
+        self.audio_dec = audio_dec = 10
+        self.RF_Gain = RF_Gain = 30
+        self.CF = CF = 107.3e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=4,
-                decimation=1,
+        _volume_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._volume_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_volume_sizer,
+        	value=self.volume,
+        	callback=self.set_volume,
+        	label="Volume",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._volume_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_volume_sizer,
+        	value=self.volume,
+        	callback=self.set_volume,
+        	minimum=0.2,
+        	maximum=1,
+        	num_steps=4,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_volume_sizer)
+        self._variable_sample_rate_0_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	value=self.variable_sample_rate_0,
+        	callback=self.set_variable_sample_rate_0,
+        	label="Sample Rate: 1.024M, 1.4M, 1.8M, 1.92M, 2.048M, 2.4M & 2. 56M",
+        	converter=forms.float_converter(),
+        )
+        self.GridAdd(self._variable_sample_rate_0_text_box, 7, 0, 1, 5)
+        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Spectrum")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Waterfall")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Constellation")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Scope")
+        self.GridAdd(self.notebook_0, 1, 0, 4, 5)
+        _RF_Gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._RF_Gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_RF_Gain_sizer,
+        	value=self.RF_Gain,
+        	callback=self.set_RF_Gain,
+        	label="RF Gain",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._RF_Gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_RF_Gain_sizer,
+        	value=self.RF_Gain,
+        	callback=self.set_RF_Gain,
+        	minimum=0,
+        	maximum=45,
+        	num_steps=45,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_RF_Gain_sizer, 6, 0, 1, 5)
+        _CF_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._CF_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_CF_sizer,
+        	value=self.CF,
+        	callback=self.set_CF,
+        	label="Center Frequency",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._CF_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_CF_sizer,
+        	value=self.CF,
+        	callback=self.set_CF,
+        	minimum=87.9e6,
+        	maximum=107.9e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_CF_sizer, 5, 0, 1, 5)
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+        	self.notebook_0.GetPage(1).GetWin(),
+        	baseband_freq=CF,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=variable_sample_rate_0,
+        	fft_size=5120,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="Waterfall Plot",
+        	size=(575, 600),
+        )
+        self.notebook_0.GetPage(1).Add(self.wxgui_waterfallsink2_0.win)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_c(
+        	self.notebook_0.GetPage(3).GetWin(),
+        	title="Scope Plot",
+        	sample_rate=variable_sample_rate_0,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.notebook_0.GetPage(3).Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.notebook_0.GetPage(0).GetWin(),
+        	baseband_freq=CF,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=variable_sample_rate_0,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        	size=(575, 600),
+        )
+        self.notebook_0.GetPage(0).Add(self.wxgui_fftsink2_0.win)
+        self.wxgui_constellationsink2_0 = constsink_gl.const_sink_c(
+        	self.notebook_0.GetPage(2).GetWin(),
+        	title="Constellation Plot",
+        	sample_rate=variable_sample_rate_0,
+        	frame_rate=5,
+        	const_size=2048,
+        	M=4,
+        	theta=0,
+        	loop_bw=6.28/100.0,
+        	fmax=0.06,
+        	mu=0.5,
+        	gain_mu=0.005,
+        	symbol_rate=variable_sample_rate_0/4.,
+        	omega_limit=0.005,
+        	size=(575, 600),
+        )
+        self.notebook_0.GetPage(2).Add(self.wxgui_constellationsink2_0.win)
+        self._variable_static_text_0_static_text = forms.static_text(
+        	parent=self.GetWin(),
+        	value=self.variable_static_text_0,
+        	callback=self.set_variable_static_text_0,
+        	label="SDR ",
+        	converter=forms.str_converter(),
+        )
+        self.GridAdd(self._variable_static_text_0_static_text, 0, 0, 1, 5)
+        self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=int(variable_sample_rate_0/quadrature),
                 taps=None,
                 fractional_bw=None,
         )
-        self.qtgui_sink_x_0 = qtgui.sink_c(
-        	2048, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	FM_freq, #fc
-        	samp_rate, #bw
-        	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=int(quadrature/1e3/audio_dec),
+                taps=None,
+                fractional_bw=None,
         )
-        self.qtgui_sink_x_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-        
-        self.qtgui_sink_x_0.enable_rf_freq(True)
-        
-        
-          
-        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + "" )
-        self.osmosdr_sink_0.set_sample_rate(1.28e6)
-        self.osmosdr_sink_0.set_center_freq(FM_freq, 0)
-        self.osmosdr_sink_0.set_freq_corr(0, 0)
-        self.osmosdr_sink_0.set_gain(160, 0)
-        self.osmosdr_sink_0.set_if_gain(160, 0)
-        self.osmosdr_sink_0.set_bb_gain(160, 0)
-        self.osmosdr_sink_0.set_antenna("", 0)
-        self.osmosdr_sink_0.set_bandwidth(1024, 0)
-          
-        self.blocks_wavfile_source_0 = blocks.wavfile_source("/home/bkbilly/Documents/HackRF-data/FM/bensound-summer.wav", True)
-        self.analog_wfm_tx_0 = analog.wfm_tx(
-        	audio_rate=samp_rate,
-        	quad_rate=samp_rate * 10,
-        	tau=25e-6,
-        	max_dev=75e3,
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, variable_sample_rate_0, cutoff, transition, firdes.WIN_HAMMING, 6.76))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((volume, ))
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=quadrature,
+        	audio_decimation=audio_dec,
         )
+        self.RTL820T = osmosdr.source( args="numchan=" + str(1) + " " + "hackrf=1" )
+        self.RTL820T.set_sample_rate(variable_sample_rate_0)
+        self.RTL820T.set_center_freq(CF, 0)
+        self.RTL820T.set_freq_corr(0, 0)
+        self.RTL820T.set_dc_offset_mode(0, 0)
+        self.RTL820T.set_iq_balance_mode(0, 0)
+        self.RTL820T.set_gain_mode(False, 0)
+        self.RTL820T.set_gain(RF_Gain, 0)
+        self.RTL820T.set_if_gain(20, 0)
+        self.RTL820T.set_bb_gain(20, 0)
+        self.RTL820T.set_antenna("", 0)
+        self.RTL820T.set_bandwidth(0, 0)
+          
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_tx_0, 0), (self.rational_resampler_xxx_0, 0))    
-        self.connect((self.blocks_wavfile_source_0, 0), (self.analog_wfm_tx_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.osmosdr_sink_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_sink_x_0, 0))    
+        self.connect((self.RTL820T, 0), (self.rational_resampler_xxx_1, 0))    
+        self.connect((self.RTL820T, 0), (self.wxgui_constellationsink2_0, 0))    
+        self.connect((self.RTL820T, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.RTL820T, 0), (self.wxgui_scopesink2_0, 0))    
+        self.connect((self.RTL820T, 0), (self.wxgui_waterfallsink2_0, 0))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.rational_resampler_xxx_1, 0), (self.low_pass_filter_0, 0))    
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
+    def get_volume(self):
+        return self.volume
 
+    def set_volume(self, volume):
+        self.volume = volume
+        self._volume_slider.set_value(self.volume)
+        self._volume_text_box.set_value(self.volume)
+        self.blocks_multiply_const_vxx_0.set_k((self.volume, ))
 
-    def get_samp_rate(self):
-        return self.samp_rate
+    def get_variable_static_text_0(self):
+        return self.variable_static_text_0
 
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.qtgui_sink_x_0.set_frequency_range(self.FM_freq, self.samp_rate)
+    def set_variable_static_text_0(self, variable_static_text_0):
+        self.variable_static_text_0 = variable_static_text_0
+        self._variable_static_text_0_static_text.set_value(self.variable_static_text_0)
 
-    def get_FM_freq(self):
-        return self.FM_freq
+    def get_variable_sample_rate_0(self):
+        return self.variable_sample_rate_0
 
-    def set_FM_freq(self, FM_freq):
-        self.FM_freq = FM_freq
-        self.osmosdr_sink_0.set_center_freq(self.FM_freq, 0)
-        self.qtgui_sink_x_0.set_frequency_range(self.FM_freq, self.samp_rate)
+    def set_variable_sample_rate_0(self, variable_sample_rate_0):
+        self.variable_sample_rate_0 = variable_sample_rate_0
+        self._variable_sample_rate_0_text_box.set_value(self.variable_sample_rate_0)
+        self.RTL820T.set_sample_rate(self.variable_sample_rate_0)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+        self.wxgui_constellationsink2_0.set_sample_rate(self.variable_sample_rate_0)
+        self.wxgui_fftsink2_0.set_sample_rate(self.variable_sample_rate_0)
+        self.wxgui_scopesink2_0.set_sample_rate(self.variable_sample_rate_0)
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.variable_sample_rate_0)
+
+    def get_transition(self):
+        return self.transition
+
+    def set_transition(self, transition):
+        self.transition = transition
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_quadrature(self):
+        return self.quadrature
+
+    def set_quadrature(self, quadrature):
+        self.quadrature = quadrature
+
+    def get_cutoff(self):
+        return self.cutoff
+
+    def set_cutoff(self, cutoff):
+        self.cutoff = cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_audio_dec(self):
+        return self.audio_dec
+
+    def set_audio_dec(self, audio_dec):
+        self.audio_dec = audio_dec
+
+    def get_RF_Gain(self):
+        return self.RF_Gain
+
+    def set_RF_Gain(self, RF_Gain):
+        self.RF_Gain = RF_Gain
+        self._RF_Gain_slider.set_value(self.RF_Gain)
+        self._RF_Gain_text_box.set_value(self.RF_Gain)
+        self.RTL820T.set_gain(self.RF_Gain, 0)
+
+    def get_CF(self):
+        return self.CF
+
+    def set_CF(self, CF):
+        self.CF = CF
+        self._CF_slider.set_value(self.CF)
+        self._CF_text_box.set_value(self.CF)
+        self.RTL820T.set_center_freq(self.CF, 0)
+        self.wxgui_fftsink2_0.set_baseband_freq(self.CF)
+        self.wxgui_waterfallsink2_0.set_baseband_freq(self.CF)
 
 
 def main(top_block_cls=top_block, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-    tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    tb.Start(True)
+    tb.Wait()
 
 
 if __name__ == '__main__':
--- morse/top_block.py
+++ morse/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Tue Dec  3 11:32:30 2019
+# Generated: Fri Jan  3 19:22:34 2020
 ##################################################
 
 if __name__ == '__main__':
@@ -16,14 +16,15 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
-from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
-from gnuradio.fft import window
 from gnuradio.filter import firdes
-from gnuradio.wxgui import scopesink2
-from gnuradio.wxgui import waterfallsink2
+from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
@@ -41,67 +42,155 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 48000
+        self.volume = volume = 0.2
+        self.variable_sample_rate_0 = variable_sample_rate_0 = 2.048E6
+        self.transition = transition = 1e6
+        self.samp_rate = samp_rate = int(32e3)
+        self.quadrature = quadrature = 500e3
+        self.hackRF_samp_rate = hackRF_samp_rate = 32e3
+        self.cutoff = cutoff = 100e3
+        self.audio_dec = audio_dec = 10
+        self.FM_freq = FM_freq = 107.9e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
-        	self.GetWin(),
-        	baseband_freq=434.5e6,
-        	dynamic_range=100,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=512,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title="Waterfall Plot",
+        self._variable_sample_rate_0_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	value=self.variable_sample_rate_0,
+        	callback=self.set_variable_sample_rate_0,
+        	label="Sample Rate: 1.024M, 1.4M, 1.8M, 1.92M, 2.048M, 2.4M & 2. 56M",
+        	converter=forms.float_converter(),
         )
-        self.Add(self.wxgui_waterfallsink2_0.win)
-        self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_c(
-        	self.GetWin(),
-        	title="Scope Plot",
-        	sample_rate=samp_rate,
-        	v_scale=1,
-        	v_offset=0,
-        	t_scale=0.06,
-        	ac_couple=False,
-        	xy_mode=False,
-        	num_inputs=1,
-        	trig_mode=wxgui.TRIG_MODE_AUTO,
-        	y_axis_label="Counts",
+        self.GridAdd(self._variable_sample_rate_0_text_box, 7, 0, 1, 5)
+        _volume_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._volume_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_volume_sizer,
+        	value=self.volume,
+        	callback=self.set_volume,
+        	label="Volume",
+        	converter=forms.float_converter(),
+        	proportion=0,
         )
-        self.Add(self.wxgui_scopesink2_0_0.win)
-        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "hackrf=1" )
-        self.osmosdr_source_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0.set_center_freq(434.5e6, 0)
-        self.osmosdr_source_0.set_freq_corr(0, 0)
-        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0.set_gain(20, 0)
-        self.osmosdr_source_0.set_if_gain(20, 0)
-        self.osmosdr_source_0.set_bb_gain(20, 0)
-        self.osmosdr_source_0.set_antenna("", 0)
-        self.osmosdr_source_0.set_bandwidth(0, 0)
+        self._volume_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_volume_sizer,
+        	value=self.volume,
+        	callback=self.set_volume,
+        	minimum=0.2,
+        	maximum=1,
+        	num_steps=4,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_volume_sizer)
+        self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=int(variable_sample_rate_0/quadrature),
+                taps=None,
+                fractional_bw=None,
+        )
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=int(quadrature/1e3/audio_dec),
+                taps=None,
+                fractional_bw=None,
+        )
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, variable_sample_rate_0, cutoff, transition, firdes.WIN_HAMMING, 6.76))
+        self.blocks_wavfile_sink_0_0 = blocks.wavfile_sink("/home/bkbilly/Desktop/morse.wav", 1, samp_rate, 8)
+        self.audio_sink_0_0 = audio.sink(samp_rate, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=quadrature,
+        	audio_decimation=audio_dec,
+        )
+        self.RTL820T = osmosdr.source( args="numchan=" + str(1) + " " + "hackrf=1" )
+        self.RTL820T.set_sample_rate(samp_rate)
+        self.RTL820T.set_center_freq(FM_freq, 0)
+        self.RTL820T.set_freq_corr(0, 0)
+        self.RTL820T.set_dc_offset_mode(0, 0)
+        self.RTL820T.set_iq_balance_mode(0, 0)
+        self.RTL820T.set_gain_mode(False, 0)
+        self.RTL820T.set_gain(30, 0)
+        self.RTL820T.set_if_gain(20, 0)
+        self.RTL820T.set_bb_gain(20, 0)
+        self.RTL820T.set_antenna("", 0)
+        self.RTL820T.set_bandwidth(0, 0)
           
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_0, 0), (self.wxgui_scopesink2_0_0, 0))    
-        self.connect((self.osmosdr_source_0, 0), (self.wxgui_waterfallsink2_0, 0))    
+        self.connect((self.RTL820T, 0), (self.rational_resampler_xxx_1, 0))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_wavfile_sink_0_0, 0))    
+        self.connect((self.rational_resampler_xxx_1, 0), (self.low_pass_filter_0, 0))    
+
+    def get_volume(self):
+        return self.volume
+
+    def set_volume(self, volume):
+        self.volume = volume
+        self._volume_slider.set_value(self.volume)
+        self._volume_text_box.set_value(self.volume)
+
+    def get_variable_sample_rate_0(self):
+        return self.variable_sample_rate_0
+
+    def set_variable_sample_rate_0(self, variable_sample_rate_0):
+        self.variable_sample_rate_0 = variable_sample_rate_0
+        self._variable_sample_rate_0_text_box.set_value(self.variable_sample_rate_0)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_transition(self):
+        return self.transition
+
+    def set_transition(self, transition):
+        self.transition = transition
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
-        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
-        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.RTL820T.set_sample_rate(self.samp_rate)
+
+    def get_quadrature(self):
+        return self.quadrature
+
+    def set_quadrature(self, quadrature):
+        self.quadrature = quadrature
+
+    def get_hackRF_samp_rate(self):
+        return self.hackRF_samp_rate
+
+    def set_hackRF_samp_rate(self, hackRF_samp_rate):
+        self.hackRF_samp_rate = hackRF_samp_rate
+
+    def get_cutoff(self):
+        return self.cutoff
+
+    def set_cutoff(self, cutoff):
+        self.cutoff = cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.variable_sample_rate_0, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_audio_dec(self):
+        return self.audio_dec
+
+    def set_audio_dec(self, audio_dec):
+        self.audio_dec = audio_dec
+
+    def get_FM_freq(self):
+        return self.FM_freq
+
+    def set_FM_freq(self, FM_freq):
+        self.FM_freq = FM_freq
+        self.RTL820T.set_center_freq(self.FM_freq, 0)
 
 
 def main(top_block_cls=top_block, options=None):
